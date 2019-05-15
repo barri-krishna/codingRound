83f930201263eab43a8ab237823bc11c07fbd55f
krishna.barri <krishna.barri1995@gmail.com>
2019-05-15 19:12:23 +0530

    Added newer versions of driver and Common library

---
+++ src/main/java/utils/CommonLibrary.java
@@ -0,0 +1,91 @@
+package utils;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.sun.javafx.PlatformUtil;
+
+/**
+ * @author krishna.barri
+ *
+ */
+@SuppressWarnings("restriction")
+public class CommonLibrary {
+
+	private WebDriver driver;
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	public void setDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	private void setDriverPath() {
+		if (PlatformUtil.isMac()) {
+			System.setProperty("webdriver.chrome.driver",
+					AppConstants.PARENTFOLDER_PATH + AppConstants.RESOURCES_CHROME_MAC_DIR);
+		}
+		if (PlatformUtil.isWindows()) {
+			System.setProperty("webdriver.chrome.driver",
+					AppConstants.PARENTFOLDER_PATH + AppConstants.RESOURCES_CHROME_WIN_DIR);
+		}
+		if (PlatformUtil.isLinux()) {
+			System.setProperty("webdriver.chrome.driver",
+					AppConstants.PARENTFOLDER_PATH + AppConstants.RESOURCES_CHROME_LINUX_DIR);
+		}
+	}
+
+	public void invokeBrowser(String requiredBrowserName) {
+		try {
+			setDriverPath();
+			if (driver == null) {
+				switch (requiredBrowserName) {
+				case AppConstants.CHROME: {
+					setDriver(new ChromeDriver());
+					break;
+				}
+				case AppConstants.FIREFOX: {
+					setDriver(new FirefoxDriver());
+					break;
+				}
+				case AppConstants.IE: {
+					setDriver(new InternetExplorerDriver());
+					break;
+				}
+				default: {
+					setDriver(new ChromeDriver());
+				}
+				}
+			}
+			getDriver().manage().window().maximize();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void waitFor(int durationInMilliSeconds) {
+		try {
+			Thread.sleep(durationInMilliSeconds);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void waitForElement(By byLocater) {
+		WebDriverWait wait = new WebDriverWait(driver, AppConstants.DRIVER_WAIT);
+		wait.until(ExpectedConditions.presenceOfElementLocated(byLocater));
+		wait.pollingEvery(AppConstants.POLLING_TIME, TimeUnit.SECONDS);
+		wait.ignoring(NoSuchElementException.class, StaleElementReferenceException.class);
+	}
+
+}
